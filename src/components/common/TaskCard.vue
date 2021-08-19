cdcbc984eb9f625c01fc0b6affbafb54bcbe6fca
Crepiks <sayazhan.onlassyn@mail.ru>
2021-08-19 17:25:15 +0600

    feat: implement displaying task cards

---
+++ src/components/common/TaskCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <article class="task-card">
+    <span class="task-card__email">{{ email }}</span>
+    <h3 class="task-card__title">{{ title }}</h3>
+    <p class="task-card__text">{{ text }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    email: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.task-card {
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+}
+
+.task-card__title {
+  margin: 4px 0 20px;
+  font-weight: 500;
+  font-size: 24px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="home">
     <bultech-header heading="Hello" @heading-change="handleHeadingChange" />
+    <div class="home__content">
+      <div class="home__tasks">
+        <task-card
+          v-for="task in tasks"
+          :key="task.id"
+          :email="task.email"
+          :title="task.title"
+          :text="task.text"
+          class="home__task-card"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "@/components/common/Header";
+import TaskCard from "@/components/common/TaskCard";
+
+const tasks = [
+  {
+    id: 1,
+    email: "sayazhan.onlassyn@mail.ru",
+    title: "Сделать тестовое задание",
+    text: "Выполнить тестовое задание от компании Bultech на позицию Senior Frontend Developer",
+  },
+  {
+    id: 2,
+    email: "sayazhan.onlassyn@mail.ru",
+    title: "Сделать тестовое задание",
+    text: "Выполнить тестовое задание от компании Bultech на позицию Senior Frontend Developer",
+  },
+];
 
 export default {
   name: "Home",
   components: {
     "bultech-header": Header,
+    "task-card": TaskCard,
   },
+  data: () => ({
+    tasks,
+  }),
   methods: {
     handleHeadingChange(value) {
       console.log(value);
@@ -19,3 +51,19 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.home__content {
+  padding: 0 40px;
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+}
+
+.home__task-card {
+  margin-bottom: 20px;
+}
+
+.home__task-card:last-child {
+  margin-bottom: 0;
+}
+</style>
